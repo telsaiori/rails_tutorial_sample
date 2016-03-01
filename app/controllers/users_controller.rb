8c98f717a9e59d719dc3889d68c14cb33b9c8066
telsaiori <telsaiori@gmail.com>
2016-03-01 07:10:00 +0000

    Listing 9.53: Adding a working destroy action.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
   
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   
   def index
     @user = User.all
+    @users = User.paginate(page: params[:page])
   end
   
   def show
@@ -54,6 +56,12 @@ class UsersController < ApplicationController
       
   
   end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User delete"
+    redirect_to users_url
+  end
 
   
   private
@@ -74,5 +82,8 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
     end
     
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
     
 end
