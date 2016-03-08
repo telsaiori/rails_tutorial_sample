a64ecf50fccafe6d3d797af6ae5f8b1f5715a371
telsaiori <telsaiori@gmail.com>
2016-03-08 03:28:22 +0000

    リスト10.47: パスワード再設定用メイラーメソッドのテストを追加する GREEN

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   root 'static_pages#home'
   get '/help'  => 'static_pages#help'
 
@@ -21,4 +25,6 @@ Rails.application.routes.draw do
   resources :users
   
   resources :account_activations, only: [:edit]
+  
+  resources :password_resets, only: [:new, :create, :edit, :update]
 end
